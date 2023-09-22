e524f13b6e5d84dac84a0e7d4e0b9b115cde4d4d
nguyen-ducduy269 <112228673+nguyen-ducduy269@users.noreply.github.com>
2023-09-22 19:06:38 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        appSchool: resolve(__dirname, "resources/school/index.html"),
-        appStudent: resolve(__dirname, "resources/student/index.html"),
-        appAuth: resolve(__dirname, "resources/auth/index.html"),
+        appSchool: resolve(__dirname, "index.html"),
+        appStudent: resolve(__dirname, "myorder.html"),
+        appAuth: resolve(__dirname, "check-out.html"),
       },
     },
   },
